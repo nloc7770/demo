d7f419e4cbac3b665f092f12035612886ae98a70
“nloc7770” <nloc7770@gmail.com>
2023-05-17 14:16:00 +0700

    update

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,11 +5,12 @@ import { apiClient } from '../lib/api';
 import { makeAllMenus } from '../lib/menu';
 
 export default function ShippingPage({ mainMenu, footerMenu }: IPageProps) {
+	
 	return (
 		<MainLayout mainMenu={mainMenu} footerMenu={footerMenu}>
 			<div className={'container-xxl'}>
 				<h1 className='page-heading page-heading_h1  page-heading_m-h1'>About</h1>
-				<div className='text-container' style={{textAlign:"center"}}>
+				<div className='text-container' style={{ textAlign: "center" }}>
 					<p >Thông Tin Kinh Doanh</p>
 					<h3 >CỬA HÀNG ĐIỆN THOẠI Apollo zone</h3>
 					<p >Địa chỉ: 100 Luỹ Bán Bích, P. Tân Thành, Q. Tân Phú. HCM</p>
